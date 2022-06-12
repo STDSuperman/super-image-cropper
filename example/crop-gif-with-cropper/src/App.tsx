fb64b939b140443cab20fcbc5a359968d1f39e91
STDSuperman <2750556766@qq.com>
2022-06-12 15:00:28 +0800

    :construction: chore(example): change the default component to react-cropper.

--- example/crop-gif-with-cropper/src/App.tsx
+++ example/crop-gif-with-cropper/src/App.tsx
@@ -1,11 +1,11 @@
 import './App.css';
 import 'cropperjs/dist/cropper.css';
 import ReactCropperJs from './components/react-cropperjs'
-import PureCropperJs from './components/pure-cropperjs'
+// import PureCropperJs from './components/pure-cropperjs'
 
 function App() {
-  // return <ReactCropperJs></ReactCropperJs>
-  return <PureCropperJs></PureCropperJs>
+  return <ReactCropperJs></ReactCropperJs>
+  // return <PureCropperJs></PureCropperJs>
 }
 
 export default App;
