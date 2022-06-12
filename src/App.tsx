11b0ea359c7925a2dea3fc9b4e479f7c95c5c264
陌小路 <44311619+STDSuperman@users.noreply.github.com>
2022-06-12 12:34:10 +0800

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css';
 import 'cropperjs/dist/cropper.css';
-import ReactCropperJs from './components/react-cropperjs'
+// import ReactCropperJs from './components/react-cropperjs'
 import PureCropperJs from './components/pure-cropperjs'
 
 function App() {
