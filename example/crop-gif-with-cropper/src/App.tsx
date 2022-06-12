5262e42b09a1c37a4bf1b272e531be45d07372e0
STDSuperman <2750556766@qq.com>
2022-06-12 12:15:23 +0800

    :sparkles: feat(example): pref example code.

--- example/crop-gif-with-cropper/src/App.tsx
+++ example/crop-gif-with-cropper/src/App.tsx
@@ -4,8 +4,8 @@ import ReactCropperJs from './components/react-cropperjs'
 import PureCropperJs from './components/pure-cropperjs'
 
 function App() {
-  return <ReactCropperJs></ReactCropperJs>
-  // return <PureCropperJs></PureCropperJs>
+  // return <ReactCropperJs></ReactCropperJs>
+  return <PureCropperJs></PureCropperJs>
 }
 
 export default App;
