52247c90ad41b515ecb99a2bb15ac0283442b1fb
xiaohang.lin <xiaohang.lin@alibaba-inc.com>
2023-01-14 17:54:44 +0800

    release(super-image-cropper): 🏹 Release:(super-image-cropper@1.0.12)

--- example/crop-gif-with-cropper/src/App.tsx
+++ example/crop-gif-with-cropper/src/App.tsx
@@ -2,7 +2,7 @@ import './App.css';
 import 'cropperjs/dist/cropper.css';
 import ReactCropperJs from './components/react-cropperjs'
 // import PureCropperJs from './components/pure-cropperjs'
-import Test1 from './components/test1';
+// import Test1 from './components/test1';
 
 function App() {
   return <ReactCropperJs></ReactCropperJs>
