9d04f1957e93f32c53b88a899ac7edb75fdfda74
STDSuperman <2750556766@qq.com>
2024-10-16 23:45:07 +0800

    :wrench: chore(custom): turbo

--- build/rollup.config.base.mjs
+++ build/rollup.config.base.mjs
@@ -1,4 +1,3 @@
-import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
 import { terser } from 'rollup-plugin-terser';
